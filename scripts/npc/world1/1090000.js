c9f6c019bbeca6d7ab47d62e20209fe038c34d0f
Martin Skow Røed <msroed@gmail.com>
2015-05-24 00:31:34 +0200

    debug

--- scripts/npc/world1/1090000.js
+++ scripts/npc/world1/1090000.js
@@ -29,27 +29,29 @@ var job = 510;
 var jobName = "Gunslinger";
 
 function start() {
-    if (cm.getJobId() == 0) {
-        if (cm.getLevel() >= 10)
-            cm.sendNext("So you decided to become a #rPirate#k?");
-        else {
-            cm.sendOk("Train a bit more and I can show you the way of the #rPirate#k.")
-            cm.dispose();
-        }
-    } else {
-        if (cm.getLevel() >= 30 && cm.getJobId() == 500) {
-            status = 10;
-            cm.sendNext("The progress you have made is astonishing.");
-        } else if (cm.getLevel() >= 70 && (cm.getJobId() == 510 || cm.getJobId() == 520))
-            cm.sendOk("Please go visit #bArec#k. He resides in #bEl Nath#k.");
-        else if (cm.getLevel() < 30 && cm.getJobId() == 500)
-            cm.sendOk("Please come back to see me once you have trained more.");
-        else if (cm.getLevel() >= 120 && cm.getJobId() > 510 && cm.getJobId()%10 == 1)
-            cm.sendOk("Please go visit the 4th job advancement person.");
-        else
-            cm.sendOk("Please let me down...");
-        cm.dispose();
-    }
+    cm.sendOk("WHAT IS UP");
+    cm.dispose();
+    // if (cm.getJobId() == 0) {
+    //     if (cm.getLevel() >= 10)
+    //         cm.sendNext("So you decided to become a #rPirate#k?");
+    //     else {
+    //         cm.sendOk("Train a bit more and I can show you the way of the #rPirate#k.")
+    //         cm.dispose();
+    //     }
+    // } else {
+    //     if (cm.getLevel() >= 30 && cm.getJobId() == 500) {
+    //         status = 10;
+    //         cm.sendNext("The progress you have made is astonishing.");
+    //     } else if (cm.getLevel() >= 70 && (cm.getJobId() == 510 || cm.getJobId() == 520))
+    //         cm.sendOk("Please go visit #bArec#k. He resides in #bEl Nath#k.");
+    //     else if (cm.getLevel() < 30 && cm.getJobId() == 500)
+    //         cm.sendOk("Please come back to see me once you have trained more.");
+    //     else if (cm.getLevel() >= 120 && cm.getJobId() > 510 && cm.getJobId()%10 == 1)
+    //         cm.sendOk("Please go visit the 4th job advancement person.");
+    //     else
+    //         cm.sendOk("Please let me down...");
+    //     cm.dispose();
+    // }
 }
 
 function action(mode, type, selection) {
