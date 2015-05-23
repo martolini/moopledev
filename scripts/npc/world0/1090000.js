f65d3f58c21b5621f26c167ed7b0faa8ef06bf5e
Martin Skow Røed <msroed@gmail.com>
2015-05-24 00:46:56 +0200

    debug

--- scripts/npc/world0/1090000.js
+++ scripts/npc/world0/1090000.js
@@ -40,7 +40,6 @@ function start() {
         if (cm.getLevel() >= 30 && cm.getJobId() == 500) {
             status = 10;
             cm.sendNext("The progress you have made is astonishing");
-            cm.dispose();
         } else if (cm.getLevel() >= 70 && (cm.getJobId() == 510 || cm.getJobId() == 520))
             cm.sendOk("Please go visit #bArec#k. He resides in #bEl Nath#k.");
         else if (cm.getLevel() < 30 && cm.getJobId() == 500)
