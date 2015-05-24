e6ab04db2fbe4ef5c30bcac104a8d0152cc050f3
Martin Skow Røed <msroed@gmail.com>
2015-05-24 20:33:58 +0200

    Spinel can now max skills

--- src/scripting/npc/NPCConversationManager.java
+++ src/scripting/npc/NPCConversationManager.java
@@ -295,6 +295,19 @@ public class NPCConversationManager extends AbstractPlayerInteraction {
         getPlayer().resetStats();
     }
 
+    public void maxSkills() {
+        for (MapleData skill_ : MapleDataProviderFactory.getDataProvider(new File(System.getProperty("wzpath") + "/" + "String.wz")).getData("Skill.img").getChildren()) {
+            try {
+                Skill skill = SkillFactory.getSkill(Integer.parseInt(skill_.getName()));
+                getPlayer().changeSkillLevel(skill, (byte) skill.getMaxLevel(), skill.getMaxLevel(), -1);
+            } catch (NumberFormatException nfe) {
+                break;
+            } catch (NullPointerException npe) {
+                continue;
+            }
+        }
+    }
+
     public void maxMastery() {
         for (MapleData skill_ : MapleDataProviderFactory.getDataProvider(new File(System.getProperty("wzpath") + "/" + "String.wz")).getData("Skill.img").getChildren()) {
             try {
